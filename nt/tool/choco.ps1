5c11d2a4da20972e16a56479d5e88b9dddcd51eb
kemono <furry@kemono.app>
2020-04-20 20:03:31 +0900

    add aimp (choco)

--- nt/tool/choco.ps1
+++ nt/tool/choco.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 C:\ProgramData\chocolatey\bin\choco.exe install -y `
-emacs git steam winrar rufus `
+emacs git steam winrar rufus aimp `
 libreoffice qbittorrent lein firefox `
 python openssh sysinternals vim `
 vcxsrv googlechrome selenium-chrome-driver `
