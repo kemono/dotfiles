6b870950452b68424fc9c308a1ca1c416d2c881d
kemono <furry@kemono.app>
2020-04-20 19:59:34 +0900

    Goooood bye iTunes !!!

--- nt/tool/choco.ps1
+++ nt/tool/choco.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 C:\ProgramData\chocolatey\bin\choco.exe install -y `
-emacs git steam itunes winrar rufus `
+emacs git steam winrar rufus `
 libreoffice qbittorrent lein firefox `
 python openssh sysinternals vim `
 vcxsrv googlechrome selenium-chrome-driver `
