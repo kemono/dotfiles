68da76f425a0071da888bb20f23e9e9124f8b603
kemono <furry@kemono.app>
2020-08-18 18:49:57 +0900

    sdl2 dll added

--- nt/tool/sbcl.ps1
+++ nt/tool/sbcl.ps1
@@ -12,8 +12,21 @@ msiexec.exe /i c:\tmp\sbcl-2.0.0-x86-64-windows-binary.msi /passive /norestart
 
 # SDL2 64bit install
 $cli = New-Object System.Net.WebClient
-$uri = New-Object System.Uri("https://www.libsdl.org/release/SDL2-2.0.9-win32-x64.zip")
+$uri = New-Object System.Uri("https://www.libsdl.org/release/SDL2-2.0.12-win32-x64.zip")
 $file = Split-Path $uri.AbsolutePath -Leaf
 $cli.DownloadFile($uri, (Join-Path "C:\tmp\" $file))
-Expand-Archive -Path C:\tmp\SDL2-2.0.9-win32-x64.zip -DestinationPath C:\tmp
+Expand-Archive -Path C:\tmp\SDL2-2.0.12-win32-x64.zip -DestinationPath C:\tmp
 Move-Item C:\tmp\SDL2.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+
+# SDL2 image 64bit install
+$cli = New-Object System.Net.WebClient
+$uri = New-Object System.Uri("https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5-win32-x64.zip")
+$file = Split-Path $uri.AbsolutePath -Leaf
+$cli.DownloadFile($uri, (Join-Path "C:\tmp\" $file))
+Expand-Archive -Path C:\tmp\SDL2_image-2.0.5-win32-x64.zip -DestinationPath C:\tmp
+Move-Item C:\tmp\libjpeg-9.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+Move-Item C:\tmp\libpng16-16.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+Move-Item C:\tmp\libtiff-5.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+Move-Item C:\tmp\libwebp-7.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+Move-Item C:\tmp\zlib1.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
+Move-Item C:\tmp\SDL2_image.dll C:\Program` Files\Steel` Bank` Common` Lisp\2.0.0
