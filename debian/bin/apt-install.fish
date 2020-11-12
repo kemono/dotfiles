9cc6ba8da73f820b20d3231c761b888357b25b7e
kemono <furry@kemono.app>
2020-11-12 16:33:36 +0900

    add transmission other packages

--- debian/bin/apt-install.fish
+++ debian/bin/apt-install.fish
@@ -2,77 +2,78 @@
 
 # default package list
 set install_packages \
-anacron \
-autoconf \
-automake \
-bison \
-build-essential \
-caca-utils \
-chromium-chromedriver \
-chromium-driver \
-clusterssh \
-cowsay \
-cpulimit \
-curl \
-default-jdk \
-default-jre \
-dnsutils \
-ffmpeg \
-fish \
-flex \
-g++ \
-gawk \
-gcc \
-gdb \
-gdebi \
-git \
-gzip \
-htop \
-httpie \
-imagemagick \
-jq \
-libcurl4-openssl-dev \
-libfreetype6-dev \
-libgif-dev \
-libglib2.0-dev \
-libgmp-dev \
-libgnutls28-dev \
-libgtk2.0-dev \
-libjansson-dev \
-libjpeg-dev \
-libmagickwand-dev \
-libncurses-dev \
-libpng-dev \
-libsoil-dev \
-libssl-dev \
-libtiff-dev \
-libtool-dev \
-libx11-dev \
-libxpm-dev \
-lynx \
-make \
-mpv \
-ncurses-dev \
-nkf \
-nmap \
-npm \
-pkg-config \
-pv \
-python3-pip \
-screen \
-silversearcher-ag \
-speedtest-cli \
-ssh \
-texinfo \
-traceroute \
-transmission \
-transmission-cli \
-vim \
-vuls \
-zip \
-zlib1g-dev \
-zsh \
-zstd
+    anacron \
+    autoconf \
+    automake \
+    bison \
+    build-essential \
+    caca-utils \
+    chromium-chromedriver \
+    chromium-driver \
+    clusterssh \
+    cowsay \
+    cpulimit \
+    curl \
+    default-jdk \
+    default-jre \
+    dnsutils \
+    ffmpeg \
+    fish \
+    flex \
+    g++ \
+    gawk \
+    gcc \
+    gdb \
+    gdebi \
+    git \
+    gzip \
+    htop \
+    httpie \
+    imagemagick \
+    jq \
+    libcurl4-openssl-dev \
+    libfreetype6-dev \
+    libgif-dev \
+    libglib2.0-dev \
+    libgmp-dev \
+    libgnutls28-dev \
+    libgtk2.0-dev \
+    libjansson-dev \
+    libjpeg-dev \
+    libmagickwand-dev \
+    libncurses-dev \
+    libpng-dev \
+    libsoil-dev \
+    libssl-dev \
+    libtiff-dev \
+    libtool-dev \
+    libx11-dev \
+    libxpm-dev \
+    lynx \
+    make \
+    mpv \
+    ncurses-dev \
+    nkf \
+    nmap \
+    npm \
+    pkg-config \
+    pv \
+    python3-pip \
+    screen \
+    silversearcher-ag \
+    speedtest-cli \
+    ssh \
+    texinfo \
+    traceroute \
+    transmission-cli \
+    transmission-daemon \
+    transmission-remote \
+    vim \
+    vuls \
+    zip \
+    zlib1g-dev \
+    zsh \
+    zstd
 
 if [ -e /usr/bin/apt ]
     set -lx DEBIAN_FRONTEND noninteractive
