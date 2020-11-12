c552a010bbb08f1b3cc979653e2bffd36cc83ea2
kemono <furry@kemono.app>
2020-11-12 17:08:37 +0900

    ssh key bind setting fixed

--- fish/config.fish
+++ fish/config.fish
@@ -27,8 +27,8 @@ transmission-daemon --download-dir ~/Downloads
 
 # ssh agent
 if test -f ~/.ssh/id_ed25519
-    eval (ssh-agent -c)
-    ssh-add ~/.ssh/id_ed25519
+    eval (ssh-agent -c) > /dev/null 2>&1
+    ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
 end
 
 ## ------------------------------------------------------------------------
