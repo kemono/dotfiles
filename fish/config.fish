6c9eb5c2ee1c0a8c13c927cdd5543b31f2bdc500
kemono <furry@kemono.app>
2020-11-13 11:20:24 +0900

    ssh-agent setting fixed

--- fish/config.fish
+++ fish/config.fish
@@ -23,6 +23,7 @@ end
 
 # ssh agent
 if test -f ~/.ssh/id_ed25519
+    killer ssh-agent > /dev/null 2>&1
     eval (ssh-agent -c) > /dev/null 2>&1
     ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
 end
