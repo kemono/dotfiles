6b71e3d14b5fddc17726082202029db850db879e
kemono <furry@kemono.app>
2020-11-25 10:35:30 +0900

    add tor browser

--- nt/tool/choco.ps1
+++ nt/tool/choco.ps1
@@ -8,6 +8,6 @@ libreoffice qbittorrent lein vivaldi `
 python sysinternals vcxsrv googlechrome `
 selenium-chrome-driver protonvpn vlc `
 openjdk12 pwsh wireshark winpcap audacity `
-azure-cli kindle dnspy
+azure-cli kindle dnspy tor-browser
 
 C:\ProgramData\chocolatey\bin\cup.exe -y all
