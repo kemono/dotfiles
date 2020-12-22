cfe9b938b181dc4dd63fd9af57f8391f9ae50d08
kemono <furry@kemono.app>
2020-12-22 18:47:52 +0900

    nt ssh install config fixed

--- nt/tool/ssh-server.ps1
+++ nt/tool/ssh-server.ps1
@@ -1,8 +1,10 @@
 # ssh server
 
 # ssh config update
+New-Item C:\ProgramData\ssh -Force -ItemType Directory
 Invoke-WebRequest https://raw.githubusercontent.com/kemono/dotfiles/master/nt/ProgramData/ssh/sshd_config -OutFile C:\ProgramData\ssh\sshd_config
-Invoke-WebRequest https://keys.kemono.app -OutFile ~/.ssh/authorized_keys
+New-Item C:/Users/Gazelle/.ssh -Force -ItemType Directory
+Invoke-WebRequest https://keys.kemono.app -OutFile C:/Users/Gazelle/.ssh/authorized_keys
 
 # default shell change
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
