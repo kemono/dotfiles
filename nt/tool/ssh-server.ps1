fa72ca460250121a1a4adc86c6e54199c4b238a7
kemono <furry@kemono.app>
2020-12-22 13:08:02 +0900

    nt ssh server setup setting fixed

--- nt/tool/ssh-server.ps1
+++ nt/tool/ssh-server.ps1
@@ -1,4 +1,12 @@
 # ssh server
+
+# ssh config update
+Invoke-WebRequest https://raw.githubusercontent.com/kemono/dotfiles/master/nt/ProgramData/ssh/sshd_config -OutFile C:\ProgramData\ssh\sshd_config
+Invoke-WebRequest https://keys.kemono.app -OutFile ~/.ssh/authorized_keys
+
+# default shell change
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+
+# service enable
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
