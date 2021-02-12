815cf3a23189b9b06c8d42335f6637ef15138feb
kemono <furry@kemono.app>
2021-02-12 17:19:29 +0900

    maintenance command updated

--- nt/bin/maintenance.ps1
+++ nt/bin/maintenance.ps1
@@ -3,3 +3,7 @@ C:\dotfiles\nt\bin\choco-update.ps1
 C:\dotfiles\nt\bin\defender-scan.ps1
 C:\dotfiles\nt\bin\repair.ps1
 C:\dotfiles\nt\bin\delete-windows-log.bat
+
+if ((Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs")) {
+    Remove-Item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -recurse -force
+}
