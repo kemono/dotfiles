4d6048e8b16be1a14f7990fa8e61240fe878a983
kemono <furry@kemono.app>
2021-03-24 09:58:50 +0900

    add vlc

--- nt/tool/choco.ps1
+++ nt/tool/choco.ps1
@@ -3,28 +3,29 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 C:\ProgramData\chocolatey\bin\choco.exe install -y `
-aimp `
-choco-cleaner `
-emacs `
-ghidra `
-git `
-irfanview `
-lein `
-libreoffice `
-openjdk `
-openssh `
-protonvpn `
-pwsh `
-python `
-qbittorrent `
-rufus `
-steam `
-sysinternals `
-vcxsrv `
-virtualbox `
-vivaldi `
-winpcap `
-winrar `
-wireshark
+  aimp `
+  choco-cleaner `
+  emacs `
+  ghidra `
+  git `
+  irfanview `
+  lein `
+  libreoffice `
+  openjdk `
+  openssh `
+  protonvpn `
+  pwsh `
+  python `
+  qbittorrent `
+  rufus `
+  steam `
+  sysinternals `
+  vcxsrv `
+  virtualbox `
+  vivaldi `
+  vlc `
+  winpcap `
+  winrar `
+  wireshark
 
 C:\ProgramData\chocolatey\bin\cup.exe -y all
