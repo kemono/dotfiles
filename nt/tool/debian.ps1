cf53b2903570548c280b1d14729bf3754b55e986
kemono <furry@kemono.app>
2021-03-26 16:25:38 +0900

    wsl install command fixed

--- nt/tool/debian.ps1
+++ nt/tool/debian.ps1
@@ -6,14 +6,16 @@ if (!(Test-Path ~\AppData\Local\Microsoft\WindowsApps\ubuntu1804.exe)) {
         Restart-Computer -Force
     }
 
-    # WSL2 install
-    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    wsl --set-default-version 2
-
     # make tmp directory
     Remove-Item -path C:\tmp -recurse -force
     New-Item C:\tmp -Force -ItemType Directory
     Set-Location C:\tmp
+
+    # WSL2 install
+    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile C:\tmp\wsl_update_x64.msi -UseBasicParsing
+    msiexec.exe /i C:\tmp\wsl_update_x64.msi /passive /norestart
+    wsl --set-default-version 2
 
     # OS install
     Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile C:\tmp\debian.Appx -UseBasicParsing
