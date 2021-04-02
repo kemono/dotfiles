981bb04f19af2454fb6574017b7496eec5939432
kemono <furry@kemono.app>
2021-04-02 16:17:53 +0900

    delete hengband

--- nt/init.ps1
+++ nt/init.ps1
@@ -5,7 +5,6 @@ C:\dotfiles\nt\tool\ssh-server.ps1
 C:\dotfiles\nt\tool\yiffalicious.ps1
 C:\dotfiles\nt\tool\sbcl.ps1
 C:\dotfiles\nt\tool\emacs.ps1
-C:\dotfiles\nt\tool\hengband.ps1
 
 C:\dotfiles\nt\bin\repair.ps1
 C:\dotfiles\nt\bin\uninstall-default-app.ps1
