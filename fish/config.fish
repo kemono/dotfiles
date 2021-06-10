4cdd154d8327a01b9a930e1bdb5a25c5abcb08cb
kemono <furry@kemono.app>
2021-06-10 15:51:09 +0900

    add fish command

--- fish/config.fish
+++ fish/config.fish
@@ -110,6 +110,7 @@ alias sl       'ls'
 alias l        'ls -AbFhlopX --time-style="+%m/%d %H:%M" -r'
 
 alias jnethack "~/nh/install/games/jnethack"
+alias spartan  "clear; curl -L raw.githubusercontent.com/kemono/dotfiles/master/etc/SpartansTheme.txt -s | lolcat"
 
 if test -f $HOME/.cargo/bin/bandwhich
     alias bw 'sudo $HOME/.cargo/bin/bandwhich'
