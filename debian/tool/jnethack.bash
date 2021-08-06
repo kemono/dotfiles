8a01e0cce5b63d81ecbab5baa0e79f179d167c2e
kemono <furry@kemono.app>
2021-08-06 10:38:57 +0900

    jnethack version up

--- debian/tool/jnethack.bash
+++ debian/tool/jnethack.bash
@@ -5,8 +5,8 @@ cd ~/tmp/dotfiles/src
 
 git clone --depth 1 -b NetHack-3.6.6_Released https://github.com/NetHack/NetHack.git
 cd NetHack
-wget https://osdn.net/projects/jnethack/downloads/72780/jnethack-3.6.6-0.1.diff.gz
-zcat jnethack-3.6.6-0.1.diff.gz | patch -p1
+wget https://osdn.net/projects/jnethack/downloads/74756/jnethack-3.6.6-0.2.diff.gz
+zcat jnethack-3.6.6-0.2.diff.gz | patch -p1
 find ./ -type f | xargs -i nkf -e -Lu --overwrite {};
 cd sys/unix
 sh setup.sh hints/linux
