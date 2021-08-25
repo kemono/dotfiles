19d0e4e8aee05ba5dbc6856307605338c61de7f8
kemono <furry@kemono.app>
2021-08-25 17:25:06 +0900

    start menu link fixed

--- nt/bin/set-start-menu-programs.ps1
+++ nt/bin/set-start-menu-programs.ps1
@@ -1,7 +1,7 @@
 
-if ((Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs")) {
-    Remove-Item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -recurse -force
-    cmd /c "mklink /D `"C:\ProgramData\Microsoft\Windows\Start Menu\Programs`" `"C:\dotfiles\nt\Start Menu\Programs`""
+if ((Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu")) {
+    Remove-Item -path "C:\ProgramData\Microsoft\Windows\Start Menu" -recurse -force
+    cmd /c "mklink /D `"C:\ProgramData\Microsoft\Windows\Start Menu`" `"C:\dotfiles\nt\Start Menu`""
 }
 
 if ((Test-Path "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs")) {
