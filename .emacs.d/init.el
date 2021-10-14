a047bb937eca4a992153beb2bef6362d20f3bd94
kemono <furry@kemono.app>
2021-10-14 11:22:04 +0900

    modeline settings updated

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,6 +79,21 @@
   (doom-themes-neotree-config)
   (doom-themes-org-config))
 
+(use-package doom-modeline
+  :custom
+  (doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (doom-modeline-icon t)
+  (doom-modeline-major-mode-icon nil)
+  (doom-modeline-minor-modes nil)
+  :hook
+  (after-init . doom-modeline-mode)
+  :config
+  (line-number-mode 0)
+  (column-number-mode 0)
+  (doom-modeline-def-modeline 'main
+    '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
+    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
+
 ;; ------------------------------------------------------------------------
 ;;                                Company
 ;; ------------------------------------------------------------------------
