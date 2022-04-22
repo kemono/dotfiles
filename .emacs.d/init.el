70b6a65017668419661d444f2629ec9db6b6e384
kemono <furry@kemono.app>
2022-04-22 21:24:48 +0900

    smartp re added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -298,6 +298,27 @@
 
 ;; Multi cursor setting
 (use-package multiple-cursors)
+(use-package smartrep
+  :config
+  (declare-function smartrep-define-key "smartrep")
+  (global-unset-key "\C-t")
+  (smartrep-define-key global-map "C-t"
+                       '(("C-t"      . 'mc/mark-next-like-this)
+                         ("n"        . 'mc/mark-next-like-this)
+                         ("p"        . 'mc/mark-previous-like-this)
+                         ("m"        . 'mc/mark-more-like-this-extended)
+                         ("u"        . 'mc/unmark-next-like-this)
+                         ("U"        . 'mc/unmark-previous-like-this)
+                         ("s"        . 'mc/skip-to-next-like-this)
+                         ("S"        . 'mc/skip-to-previous-like-this)
+                         ("*"        . 'mc/mark-all-like-this)
+                         ("d"        . 'mc/mark-all-like-this-dwim)
+                         ("i"        . 'mc/insert-numbers)
+                         ("o"        . 'mc/sort-regions)
+                         ("O"        . 'mc/reverse-regions)))
+  :bind
+  (("C-M-c" . 'mc/edit-lines)
+   ("C-M-r" . 'mc/mark-all-in-region)))
 
 ;; Vim mode
 (use-package evil
