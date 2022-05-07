180973a3873cd86e7c36b66bdc57d6d897344fce
kemono <furry@kemono.app>
2022-05-07 11:19:18 +0900

    add typescript

--- debian/bin/npm-install.fish
+++ debian/bin/npm-install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set install_package n8n
+set install_package n8n typescript
 
 for package in $install_package
     sudo -EH npm install $package -g --unsafe-perm=true --allow-root
