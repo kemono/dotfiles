688697f94f73572a8a15539e952baa6f250aaf53
kemono <furry@kemono.app>
2022-05-14 20:44:32 +0900

    npm package added

--- debian/bin/npm-install.fish
+++ debian/bin/npm-install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set install_package n8n typescript
+set install_package n8n typescript eslint tern
 
 for package in $install_package
     sudo -EH npm install $package -g --unsafe-perm=true --allow-root
