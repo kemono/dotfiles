0a3c92a3b08721bbc5dac8b3135a7b3ce6cf703a
kemono <furry@kemono.app>
2022-05-15 13:11:22 +0900

    typescript lsp added

--- debian/bin/npm-install.fish
+++ debian/bin/npm-install.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/env fish
 
-set install_package n8n typescript eslint tern
+set install_package \
+    eslint \
+    n8n \
+    tern \
+    typescript \
+    typescript-language-server
 
 for package in $install_package
     sudo -EH npm install $package -g --unsafe-perm=true --allow-root
