eadae949720679eb69415824612324d5618b629b
kemono <furry@kemono.app>
2023-10-04 22:00:23 +0900

    mini fixed

--- nt/bin/maintenance.ps1
+++ nt/bin/maintenance.ps1
@@ -3,4 +3,3 @@ C:\dotfiles\nt\bin\choco-update.ps1
 C:\dotfiles\nt\bin\defender-scan.ps1
 C:\dotfiles\nt\bin\repair.ps1
 C:\dotfiles\nt\bin\delete-windows-log.bat
-C:\dotfiles\nt\bin\set-start-menu-programs.ps1
