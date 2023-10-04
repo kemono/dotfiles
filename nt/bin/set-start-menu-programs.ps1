066a4d140472d4184bd49a4f6b6a4011677a8009
kemono <furry@kemono.app>
2023-10-04 20:56:06 +0900

    remove unused scripts

--- nt/bin/set-start-menu-programs.ps1
+++
@@ -1,9 +0,0 @@
-
-if ((Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu")) {
-    Remove-Item -path "C:\ProgramData\Microsoft\Windows\Start Menu" -recurse -force
-    cmd /c "mklink /D `"C:\ProgramData\Microsoft\Windows\Start Menu`" `"C:\dotfiles\nt\Start Menu`""
-}
-
-if ((Test-Path "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs")) {
-    Remove-Item -path "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs" -recurse -force
-}
--- nt/init.ps1
+++ nt/init.ps1
@@ -1,10 +1,8 @@
 C:\dotfiles\nt\tool\choco.ps1
-C:\dotfiles\nt\tool\ssh-server.ps1
 C:\dotfiles\nt\tool\sbcl.ps1
 C:\dotfiles\nt\tool\emacs.ps1
 C:\dotfiles\nt\tool\space-cadet.ps1
 
 C:\dotfiles\nt\bin\repair.ps1
-C:\dotfiles\nt\bin\set-start-menu-programs.ps1
 C:\dotfiles\nt\bin\set-hosts.ps1
 C:\dotfiles\nt\bin\link.ps1
