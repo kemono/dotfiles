5d67357133671ab228d9d5e0ad5c2ba511dc95d1
kemono <furry@kemono.app>
2023-10-07 21:57:28 +0900

    delete aimp

--- nt/tool/choco.ps1
+++ nt/tool/choco.ps1
@@ -3,7 +3,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 C:\ProgramData\chocolatey\bin\choco.exe install -y `
-  aimp `
   choco-cleaner `
   discord `
   dnspy `
