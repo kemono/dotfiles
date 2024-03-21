2195bf774da379e7b4772cb0bc6af117b85c4ffe
kemono <furry@kemono.app>
2024-03-21 17:38:56 +0900

    Update maintenance.ps1

--- nt/bin/maintenance.ps1
+++ nt/bin/maintenance.ps1
@@ -1,6 +1,7 @@
-C:\dotfiles\nt\bin\windows-update.ps1
-C:\dotfiles\nt\bin\choco-update.ps1
-C:\dotfiles\nt\bin\defender-scan.ps1
-C:\dotfiles\nt\bin\repair.ps1
-C:\dotfiles\nt\bin\delete-windows-log.bat
+C:\dotfiles\nt\bin\windows-update.ps1;
+C:\dotfiles\nt\bin\choco-update.ps1;
+rm -Force -Recurse ~\AppData\Local\Temp\*;
+C:\dotfiles\nt\bin\defender-scan.ps1;
+C:\dotfiles\nt\bin\repair.ps1;
+C:\dotfiles\nt\bin\delete-windows-log.bat;
 C:\dotfiles\nt\bin\empty.exe *;
