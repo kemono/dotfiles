47ba74444652a86a5a5d1623194f1e274bded32c
kemono <furry@kemono.app>
2025-10-21 17:32:58 +0900

    yt-dlp install setting added

---
+++ debian/tool/yt-dlp.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+mkdir -p ~/.local/bin
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
