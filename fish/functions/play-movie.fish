e5a4a6f681b4c970a73ce12fcc6fd450ef927dbb
kemono <furry@kemono.app>
2026-05-24 21:41:34 +0900

    play-movie function updated

--- fish/functions/play-movie.fish
+++ fish/functions/play-movie.fish
@@ -10,16 +10,20 @@ function play-movie
     set video_url (yt-dlp -g 'https://www.youtube.com/watch?v='$argv[1])
 
     set MPV_HOME ~/.config/mpv
-    env --unset=DISPLAY \
+
+    # Use '-u DISPLAY' for cross-platform compatibility.
+    # Embed OS detection directly inside the --vo argument.
+    env -u DISPLAY \
         mpv \
         --border=no \
         --loop=yes \
         --speed=$play_speed \
         --really-quiet=yes \
-        --vo=caca \
+        --vo=(if test (uname) = "Darwin"; echo tct; else; echo caca; end) \
         $video_url \
         2>/dev/null
 
+    # Clean up variables
     set -e MPV_HOME
     set -e video_url
     set -e play_speed
